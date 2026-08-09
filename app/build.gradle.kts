60e16e06e327368a4d8d62e69bbfb8cb3ea6f29a
Ömer Faruk Nehir <92220159+omerfaruknehir@users.noreply.github.com>
2026-08-09 22:08:41 +0300

    Bump Xylune to 0.24.28

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -224,8 +224,8 @@ android {
         applicationId = "app.xylune.chat"
         minSdk = 26
         targetSdk = 36
-        versionCode = 216
-        versionName = "0.24.27"
+        versionCode = 217
+        versionName = "0.24.28"
         buildConfigField("String", "SOURCE_REPOSITORY", "\"$sourceRepository\"")
         buildConfigField("String", "SOURCE_COMMIT", "\"$sourceCommit\"")
         buildConfigField("String", "MICROSOFT_CLIENT_ID", "\"$microsoftClientId\"")
