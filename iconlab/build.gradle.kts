c5b6cab963b40a99b9c7bfc19f72ea89bccbf844
Ömer Faruk Nehir <92220159+omerfaruknehir@users.noreply.github.com>
2026-08-11 02:31:08 +0300

    Bump Icon Lab test version

--- iconlab/build.gradle.kts
+++ iconlab/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "app.turp.icontest"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     buildTypes {
